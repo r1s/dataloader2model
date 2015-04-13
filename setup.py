36824f15007cd27c361fbd09fd54586f335aa992
Sukhikh Roman <roman87.r1s@gmail.com>
2015-04-13 08:49:13 +0300

    Change package name in setup

--- setup.py
+++ setup.py
@@ -17,9 +17,9 @@ version_breads = datetime.now().strftime('%y%m%d%H%M%S')
 version = '0.0.1'
 release = '1'
 
-setup(name='data_mapper',
+setup(name='dataloader2model',
       version='{0}-{1}-{2}'.format(version, release, version_breads),
-      description="r1s's data mapper",
+      description="r1s's data loader to model",
       author='r1s',
       author_email='roman87.r1s@gmail.com',
       packages=find_packages(exclude=['ez_setup']),
